6735af6f92141d42b38cfc0e77cef3317061f0f5
= <=>
2017-01-03 00:06:54 +0530

    Database config

--- djpractice/todo/admin.py
+++ djpractice/todo/admin.py
@@ -1,1 +1,31 @@
 from django.contrib import admin
+from .models import Task
+
+
+def register(mdl):
+
+    class MdlAdmin(admin.ModelAdmin):
+        list_display = ['__str__'] + getattr(mdl, 'admin_method', []) + [i.name for i in mdl._meta.fields]
+        filter_horizontal = [i.name for i in mdl._meta.many_to_many]
+
+    if hasattr(mdl, 'Admin'):
+        if hasattr(mdl.Admin, 'inlines'):
+            mdl.Admin.inlines = [get_tabular(globals()[i]) for i in mdl.Admin.inlines]
+        class NewMdlAdmin(mdl.Admin, MdlAdmin):
+            pass
+        admin.site.register(mdl, NewMdlAdmin)
+
+    else:
+        admin.site.register(mdl, MdlAdmin)
+
+
+def get_tabular(mdl):
+    class MdlInline(admin.TabularInline):
+        model = mdl
+    return MdlInline
+
+
+for i in [
+        Task,
+]:
+    register(i)
--- djpractice/todo/models.py
+++ djpractice/todo/models.py
@@ -1,2 +1,35 @@
 from __future__ import unicode_literals
 from django.db import models
+
+
+class BaseContent(models.Model):
+
+    ACTIVE_CHOICES = ((0, 'Inactive'), (2, 'Active'),)
+    active = models.PositiveIntegerField(choices=ACTIVE_CHOICES,
+                                         default=2)
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+    def switch(self):
+        self.active = {2: 0, 0: 2}[self.active]
+        self.save()
+
+
+class Task(BaseContent):
+
+    STATE_CHOICES = ((0, 'ToDo'), (1, 'Doing'), (2, 'Done'))
+    PRIORITY_CHOICES = ((0, 'Low'), (1, 'Medium'), (2, 'High'))
+
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    state = models.PositiveIntegerField(choices=STATE_CHOICES,
+                                        default=2)
+    priority =  models.PositiveIntegerField(choices=PRIORITY_CHOICES,
+                                            default=2)
+    due_date = models.DateTimeField(blank=True, null=True)
+
+    def __unicode__(self):
+        return self.name
