aa3d891e11ae408ed63fde53a0ef16ccb43735a6
Vinayak Kaniyarakkal <vinayak.k@brisatech.com>
2017-01-02 20:15:03 +0530

    Integrated design

--- djpractice/djpractice/settings.py
+++ djpractice/djpractice/settings.py
@@ -14,6 +14,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'todo',
 ]
 
 MIDDLEWARE = [
@@ -59,4 +61,7 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 USE_L10N = True
 USE_TZ = True
+
+
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (BASE_DIR + '/static/',)
